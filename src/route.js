e0802d398aae201d9d114829ff3e7a150ab3bf56
wuchm8 <32164318+wuchm8@users.noreply.github.com>
2018-09-02 13:53:26 +0800

    'handlebars'

--- src/route.js
+++ src/route.js
@@ -1,11 +1,15 @@
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
+const handlebars = require('handlebars')
 const { root } = require('./config')
+
+const tplDir = path.join(__dirname, './template/template.tpl')
+const source = fs.readFileSync(tplDir)
+// console.log(path.resolve('./template/template.tpl'))
 
 const server = http.createServer((req, res) => {
   let filePath = path.join(root, req.url)
-  // console.log(filePath)
   fs.stat(filePath, (err, stats) => {
     if(err) {
       res.writeHead(404, { contentType: 'text/plain' })
@@ -21,15 +25,24 @@ const server = http.createServer((req, res) => {
     if(stats.isDirectory()) {
       fs.readdir(filePath, (err, files) => {
         res.writeHead(200, { contentType: 'text/html'})
-        files.forEach((fileName) => {
-          res.write(`${fileName}\n`)
-        })
-        res.end()
+        // files.forEach((fileName) => {
+        //   res.write(`${fileName}\n`)
+        // })
+        let data = {
+          files,
+          title: path.basename(filePath),
+          dir: filePath
+        }
+        
+        let template = handlebars.compile(source.toString())
+        let html = template(data)
+        // console.log(html)
+        res.end(html)
       })
     }
   })
-  // res.writeHead(200, { 'Content-Type': 'text/plain' })
-  // res.end(filePath)
 })
 
 server.listen(3000);
+
+console.log('anydoor has been started')
---
+++ src/template/template.tpl
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>{{ title }}</title>
+  <style type="text/css">
+    a {
+      display: block;
+      margin: 10px;
+      font-size: 20px;
+      text-decoration: none;
+    }
+  </style>
+</head>
+<body>
+  {{#each files}}
+    <a href="/{{this}}">{{this}}</a>
+  {{/each}}
+</body>
+</html>
