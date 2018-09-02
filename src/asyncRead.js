e9cbe8eb6d9775b47204e3ee23e1a4b728d4d35d
wuchm8 <32164318+wuchm8@users.noreply.github.com>
2018-09-02 22:01:10 +0800

    'mime-contentType'

--- src/asyncRead.js
+++ src/asyncRead.js
@@ -5,6 +5,8 @@ const stat = promisify(fs.stat)
 const readdir = promisify(fs.readdir)
 const handlebars = require('handlebars')
 const { root } = require('./config')
+// const mime = require('./mime')
+const { type } = require('./mime')
 
 const tplDir = path.join(__dirname, './template/template.tpl')
 const source = fs.readFileSync(tplDir)
@@ -13,13 +15,11 @@ async function read(req, res) {
   const { url } = req
   if(url === '/favicon.ico') return
   let filePath = path.join(root, url)
-
   try{
     let stats = await stat(filePath)
     if(stats.isFile()) {
-      res.writeHead(200, { contentType: 'text/html'})
+      res.writeHead(200, { contentType: type[path.extname(url)]})
       fs.createReadStream(filePath).pipe(res)
-      // res.end()
     }
     if(stats.isDirectory()) {
       readdir(filePath)
---
+++ src/mime.js
@@ -0,0 +1,17 @@
+
+type = {
+  ".html": 'text/html',
+  ".txt": 'text/plain',
+  ".xml": 'text/xml',
+  ".gif": 'image/gif',
+  ".js": 'text/html',
+  ".jpg": 'image/jpeg',
+  ".png": 'image/png',
+  ".xhtml": 'application/xhtml',
+  ".json": 'application/json',
+  ".pdf": 'application/pdf',
+  ".docx": 'application/msword'
+}
+
+// module.exports = 
+exports.type = type
